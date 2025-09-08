30344b636c14a5052ba3e5ba3d0e291c7d4b9680
Emily Nguyen <nguy1133@msu.edu>
2025-09-07 21:25:23 -0400

    Added lexer and started lexer analyzer

--- project-1-nguy1133-main/Project1.cpp
+++ project-1-nguy1133-main/Project1.cpp
@@ -7,9 +7,54 @@
 #include <vector>
 
 // @Instructor note: Uncomment the line below once you have setup your lexer!
-// #include "lexer.hpp"
+#include "lexer.hpp"
 
 // @Instructor note: You may want to build a StringStack class to manage the translation process.
+using emplex::Lexer;
+using emplex::Token;
+
+class StringStack {
+private:
+  std::string filename;
+  Lexer lexer;
+
+  void ProcessPrint() {
+    lexer.Use(Lexer::ID_PRINT, "Internal Compiler Error!");
+    Token token = lexer.Use();
+    if (token == Lexer::ID_STRING) {
+      std::cout << "string found" << std::endl;
+      std::cout << token << std::endl;
+    }
+    else {
+      std::cerr << "Unexpected token '" << token.lexeme << '\n';
+    }
+  }
+
+  void ProcessLine() {
+    auto token = lexer.Peek();
+    switch (token) {
+      case Lexer::ID_PRINT: 
+        std::cout << "Going to print string." << std::endl;
+        ProcessPrint(); 
+        break;
+      default: lexer.Error("Unknown token '", token.lexeme, "'.");
+    }
+  }
+
+public:
+  StringStack(std::string filename) : filename(filename) { }
+
+  void Run() {
+    std::ifstream fs(filename);
+    lexer.Tokenize(fs);
+
+    while (lexer.Any()) {
+      std::cout << "Going to process line." << std::endl;
+      ProcessLine();
+    }
+  }
+};
+
 
 int main(int argc, char * argv[])
 {
@@ -21,4 +66,6 @@ int main(int argc, char * argv[])
   const std::string filename = argv[1];
 
   std::cout << "Replace me with a StringStack interpreter!!" << std::endl;
+  StringStack lang(filename);
+  lang.Run();
 }
